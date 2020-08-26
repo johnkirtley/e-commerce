403dcd688af1d53795a3e52653dc050d66219941
johnkirtley <kirtleyj16@gmail.com>
2020-08-26 15:21:58 -0700

    :fire: Added Header + Cart Count

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export const Home = () => {
-	return <h1>Home Page</h1>;
+const Home = (props) => {
+	return (
+		<div className='header-container'>
+			<div className='header-title'>
+				<h1>E-Commerce Store</h1>
+			</div>
+			<div className='checkout-logo'>
+				<img
+					src='https://cdn1.iconfinder.com/data/icons/interface-elements-iii-1/512/Basket-512.png'
+					alt='checkout cart'
+				/>
+				<span className={props.cartCount > 0 ? 'cart-count' : ''}>
+					{props.cartCount > 0 ? props.cartCount : ''}
+				</span>
+			</div>
+		</div>
+	);
 };
+
+const mapStateToProps = (state) => {
+	return {
+		cartCount: state.cartCount,
+	};
+};
+
+export default connect(mapStateToProps)(Home);
